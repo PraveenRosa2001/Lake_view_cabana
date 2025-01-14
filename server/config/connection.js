71c7f830e9cbb2c839334d27f5fce4732a2b84e0
Chandula Samarasinghe <Chandulas1999@gmail.com>
2025-01-14 11:55:59 +0530

    Refactor MongoDB connection in connection.js for improved configuration and clarity

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,18 +1,12 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const dbURI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/hoteltransylvania"; // Use the fallback if no URI is provided
-
-mongoose
-  .connect(dbURI, {
-    // Removed deprecated options
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-  });
+mongoose.connect(
+  process.env.MONGODB_URI, // || "mongodb://127.0.0.1:27017/hoteltransylvania",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 module.exports = mongoose.connection;
