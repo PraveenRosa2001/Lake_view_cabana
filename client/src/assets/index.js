d97bf1d93db2b51b4c1d5ed1618b52f0ebc74e15
Chandula Samarasinghe <Chandulas1999@gmail.com>
2025-02-10 05:28:56 +0530

    Add server setup and menu route; update client routing for menu component

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,4 +1,4 @@
-import logo from "./malieLogo.png";
+import logo from "./logo.png";
 import close from "./close.svg";
 import menu from "./menu.svg";
 import lux from "./lux.mp4";
@@ -6,6 +6,6 @@ import words from "./words.png";
 import main1 from "./main1.JPG";
 import main2 from "./main1.JPG";
 import main3 from "./main1.JPG";
-import Come from "./Come.JPG"
+import Come from "./Come.JPG";
 
-export { logo, close, menu, lux, words, main1,main2,main3 , Come};
+export { logo, close, menu, lux, words, main1, main2, main3, Come };
